46dc3bf0039983eee3be0bcc70248a53c0032d2e
Thiago Avancini <thiagoavancini@outlook.com>
2018-05-22 11:18:49 -0300

    alteração

--- SistemaFutebol/src/sistemafutebol/ComissaoTecnica.java
+++ SistemaFutebol/src/sistemafutebol/ComissaoTecnica.java
@@ -9,24 +9,41 @@ import javax.swing.JOptionPane;
  */
 public class ComissaoTecnica {
     
+    
+    
+    int contador = 0;
     public static void listar(){
         String jogador = "";
-        for(int i = 0; i < 30; i++){
-            texto += Atletas.nomes[i] + "\n"
+        for(int i = 0; i < nomes.length; i++){
+            jogador += Atletas.nomes[i] + "\n";
         }
-        JOptionPane.showMessageDialog(null, texto);
+        JOptionPane.showMessageDialog(null, jogador);
     }
     
     public static void buscarPelaPosicao(){
         String buscaPosicao = JOptionPane.showInputDialog("Informe a posicao do jogador");
         String posicaoBuscada = "";
-        for(int j = 0; j < 30; j++){
+        for(int j = 0; j < Atletas.nomes.length; j++){
             if(Atletas.posicoes[j].contains(buscaPosicao)){
                 posicaoBuscada += Atletas.nomes[j] + "\n";
             }
         }
         JOptionPane.showMessageDialog(null, posicaoBuscada);
     }
+    
+    int[] golsMarcados = new int[Atletas.nomes.length];
+    
+    public void cadastrarDesempenho(int contador){
+        golsMarcados[contador] = Integer.parseInt(JOptionPane.showInputDialog("Informe o número de gols marcados pelo jogador: " + Atletas.nomes[contador])
+        );
+        
+        
+        
+        contador++;
+        
+    }
+    
+    
     
     
     

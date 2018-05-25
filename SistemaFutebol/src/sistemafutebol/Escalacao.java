5663f8be5db82b2b3c672276a1511d678e0b8f7c
Gustavo Rodrigues <gugaaroodrigues@gmail.com>
2018-05-25 10:38:52 -0300

    iniciação da classe Escalação

---
+++ SistemaFutebol/src/sistemafutebol/Escalacao.java
@@ -0,0 +1,30 @@
+
+package sistemafutebol;
+
+import javax.swing.JOptionPane;
+
+
+// @author Gustavo Rodrigues
+
+public class Escalacao {
+    public void gerenciarEscalacao() {
+        int submenuEscalacao = JOptionPane.showOptionDialog(null, "Selecione uma opção",
+                "SISTEMA FUTEBOL", 0, JOptionPane.INFORMATION_MESSAGE, null, new Object[]{
+                "Montar",
+                "Jogos" 
+                }, "");
+        switch (submenuEscalacao) {
+            case 0:
+                
+                break;
+            case 1:
+                
+                break;
+            case 2:
+                
+        }
+      
+        }
+    
+    public void 
+}
